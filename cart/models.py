a394fe00028093091912f5d58228c6131b7abe36
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-04-19 12:35:16 -0400

    implement and test data model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,36 @@
+from decimal import Decimal
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1024)
+    vendor = models.CharField(max_length=255) # In production, this would most likely be a foreign key to a vendor table.
+    # pic = models.ImageField(upload_to='static') TODO implement image uploads.
+    price = models.DecimalField(max_digits=16, decimal_places=2) # Use decimal for currency for arithmetic precision.
+
+class Cart(models.Model):
+    user = models.CharField(max_length=32) # In production, this would most likely be a foreign key to a user table.
+
+    @property
+    def count(self) -> int:
+        return sum(map(lambda item: item.quantity, self.item_set.all()))
+
+    @property
+    def total(self) -> Decimal:
+        return sum(map(lambda item: item.price, self.item_set.all()))
+
+    @property
+    def items(self) -> models.QuerySet:
+        return self.item_set.all()
+
+    def add(self, product: Product, quantity: int) -> None:
+        self.item_set.create(product=product, quantity=quantity)
+
+class Item(models.Model):
+    cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.DO_NOTHING)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def price(self) -> Decimal:
+        return self.product.price * self.quantity
--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,36 @@
+from decimal import Decimal, getcontext
 from django.test import TestCase
 
-# Create your tests here.
+from cart.models import Cart, Product
+
+class ModelTests(TestCase):
+
+    def setUp(self):
+        context = getcontext()
+        context.prec = 2
+        self.product = Product.objects.create(name='test', description='test', vendor='test', price=999.99)
+        self.cart = Cart.objects.create(user='test')
+
+    def test_empty_cart_has_zero_count(self):
+        self.assertEqual(0, self.cart.count)
+
+    def test_empty_cart_has_zero_total(self):
+        self.assertEqual(0, self.cart.total)
+    
+    def test_non_empty_cart_has_expected_count(self):
+        count = self.cart.count
+        self.cart.add(self.product, 1)
+        self.assertEquals(count + 1, self.cart.count)
+    
+    def test_non_empty_cart_has_expected_total(self):
+        self.cart.add(self.product, 2)
+        self.assertEqual(Decimal(999.99) * 2, self.cart.total)
+    
+    def test_updating_item_quantity_has_expected_effect_on_cart_count_and_total(self):
+        self.cart.add(self.product, 1)
+        count = self.cart.count
+        item = self.cart.items.first()
+        item.quantity = 2
+        item.save()
+        self.assertEqual(count + 1, self.cart.count)
+        self.assertEqual(Decimal(999.99) * 2, self.cart.total)

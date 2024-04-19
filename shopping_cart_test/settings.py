0fe031aa203c9dedaaeacfaf4aa8ef73a6d87feb
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-04-19 11:46:00 -0400

    install cart app

--- shopping_cart_test/settings.py
+++ shopping_cart_test/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

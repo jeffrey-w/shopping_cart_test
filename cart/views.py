afc996191db9dd2efcafa1270c04f8511ee7dfdb
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-04-19 19:34:40 -0400

    implement initial view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,9 @@
+from django.views.decorators.http import require_GET
 from django.shortcuts import render
 
-# Create your views here.
+from cart.models import Cart
+
+@require_GET
+def index(request):
+    # In production, we would require an authenticated user, and query our cart table with request.user.
+    return render(request, 'index.html', context={'cart': Cart.objects.get(user='user')})

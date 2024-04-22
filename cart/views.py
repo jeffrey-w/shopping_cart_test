3d30bf600559fac084e344b7bace5c09f60cc8e9
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-04-22 10:04:19 -0400

    set tax strategy in view instead of template

--- cart/views.py
+++ cart/views.py
@@ -5,5 +5,6 @@ from cart.models import Cart
 
 @require_GET
 def index(request):
-    # In production, we would require an authenticated user, and query our cart table with request.user.
-    return render(request, 'index.html', context={'cart': Cart.objects.get(user='user')})
+    # In production, we would require an authenticated user, and would retrieve
+    # their identity and location from the HTTP request.
+    return render(request, 'index.html', context={'cart': Cart.objects.get(user='user'), 'location': 'QC'})

c62dc5f4960fe745f66791e10ea3ff8d15a0ebda
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-04-22 10:03:15 -0400

    rename file

--- cart/templatetags/cart_extras.py
+++ cart/templatetags/cart_extras.py
@@ -4,8 +4,12 @@ from django.db.models.base import settings
 register = template.Library()
 
 @register.filter
-def add_decimals(value, arg):
-    return value + arg
+def addnum(first, second):
+    return first + second
+
+@register.filter
+def addstr(first, second):
+    return str(first) + str(second)
 
 @register.simple_tag
 def tax(value, location):

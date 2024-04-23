84ab33efede3e673b51206634e40e2f2cc60e69b
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-04-23 11:28:46 -0400

    redirect '/' to cart

--- shopping_cart_test/urls.py
+++ shopping_cart_test/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda req: redirect('/cart/')),
     path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
 ]
